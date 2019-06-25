374cf29cc66c346b80ddb2e4ca984ccab7ba19bf
Mark Anthony <markanthonymartinez9@gmail.com>
2019-06-25 18:52:30 -0500

    Implement collision detection

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 extern crate winapi;
 
 use std::ptr;
-use std::str;
 use winapi::ctypes;
-use winapi::shared::minwindef;
 use winapi::shared::ntdef;
-use winapi::um::{ wincon, winuser, winnt, wincontypes, winbase };
-use std::ffi::CString;
+use winapi::um::{ wincon, winuser, winnt, wincontypes };
 use std::time::{ Instant };
+use widestring::U16CString;
 
 fn main() {
     let buff_width = 120;
@@ -29,20 +27,20 @@ fn main() {
     map += "#..............#";
     map += "#..............#";
     map += "#..............#";
+    map += "#..........#...#";
+    map += "#..........#...#";
     map += "#..............#";
     map += "#..............#";
     map += "#..............#";
     map += "#..............#";
     map += "#..............#";
     map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
+    map += "#.......########";
     map += "#..............#";
     map += "#..............#";
     map += "################";
 
-    let mut map_slice = &map.as_bytes(); //index-able reference slice
+    let map_slice = &map.as_bytes(); //index-able reference slice
 
     let buff_coord = wincontypes::COORD {
         X: 0,
@@ -51,24 +49,15 @@ fn main() {
 
     let mut window_buffer: Vec<ctypes::wchar_t> = vec!['*' as u16; buff_width * buff_height];
     let buffer_ptr = window_buffer.as_ptr();
-
-    //ignore
-    // let test = ["*".as_bytes(); 5 * 5];
-    //
-    // let utf8 = str::from_utf8(test[0]).unwrap();
-    // let utf16: Vec<u16> = utf8.encode_utf16().collect();
-    // let utf_string = String::from_utf16(&utf16).unwrap();
-    // let utf_len = utf_string.len();
-    //
-    // let c_str = CString::new(utf_string).unwrap();
-    // let ptr: *const u16 = c_str.as_ptr() as *const u16;
-
-    // let dwBytesWritten: *mut u32 = 0 as *mut u32;
+    let buff_sec_ptr = window_buffer.as_mut_ptr();
 
     let mut dw_bytes_written = 0;
 
     let mut time_point_1 = Instant::now();
     let mut time_point_2;
+
+    let mut index_val = player_y as u32 * MAP_WIDTH + player_x as u32;
+    let mut index_val_con = index_val as usize;
 
     unsafe {
         let hconsole = wincon::CreateConsoleScreenBuffer(winnt::GENERIC_READ | winnt::GENERIC_WRITE, 0, ptr::null(), wincon::CONSOLE_TEXTMODE_BUFFER, ntdef::NULL);
@@ -77,12 +66,11 @@ fn main() {
 
 
         loop {
+            //Duration measurement to time frame speed
             time_point_2 = Instant::now();
             let elapsed_time = time_point_2.duration_since(time_point_1);
             let in_nano = elapsed_time.as_micros() as f64 / 100_000.0;
-            // let in_nano_con = in_nano / 100_000.0;
             time_point_1 = time_point_2;
-            // println!("{:?}", in_nano);
 
             //Controls
             //Handle CCW Rotation
@@ -100,19 +88,41 @@ fn main() {
             }
 
             if key_trigger_w != 0 && key_trigger_w == -32768 {
-                player_x += player_a.sin() * 5.0 * in_nano;
-                player_y += player_a.cos() * 5.0 * in_nano;
+                player_x += player_a.sin() * 0.5 * in_nano;
+                player_y += player_a.cos() * 0.5 * in_nano;
+
+                index_val = player_y as u32 * MAP_WIDTH + player_x as u32;
+                index_val_con = index_val as usize;
+
+                if map_slice[index_val_con] as char == '#' {
+                    player_x -= player_a.sin() * 0.5 * in_nano;
+                    player_y -= player_a.cos() * 0.5 * in_nano;
+                }
             }
 
             if key_trigger_s != 0 && key_trigger_s == -32768 {
-                player_x -= player_a.sin() * 5.0 * in_nano;
-                player_y -= player_a.cos() * 5.0 * in_nano;
+                player_x -= player_a.sin() * 0.5 * in_nano;
+                player_y -= player_a.cos() * 0.5 * in_nano;
+
+                index_val = player_y as u32 * MAP_WIDTH + player_x as u32;
+                index_val_con = index_val as usize;
+
+                if map_slice[index_val_con] as char == '#' {
+                    player_x += player_a.sin() * 0.5 * in_nano;
+                    player_y += player_a.cos() * 0.5 * in_nano;
+                }
             }
+
+            //Displays player coordinates on screen
+            let w = format!("X: {:.2} Y: {:.2} A: {:.2}", &player_x, &player_y, &player_a);
+            let w_string = U16CString::from_str(w).unwrap();
+            let s_ptr = w_string.as_ptr();
 
             for i in 0..buff_width {
                 //For each column, calculate the projected ray angle into world space
                 let ray_angle = (player_a - player_fov / 2.0) + (i as f64 / buff_width as f64) * player_fov;
 
+                // Find distance to wall
                 let mut distance_to_wall = 0.0;
                 let mut hit_wall = false;
 
@@ -125,16 +135,16 @@ fn main() {
                     let test_x = player_x + eye_x * distance_to_wall;
                     let test_y = player_y + eye_y * distance_to_wall;
 
-                    let test_x_con = test_x as u32;
-                    let test_y_con = test_y as u32;
+                    let test_x_con = test_x as i32;
+                    let test_y_con = test_y as i32;
 
                     //Test if ray is out of bounds
-                    if test_x_con < 0 || test_x_con >= MAP_WIDTH || test_y_con < 0 || test_y_con >= MAP_HEIGHT {
+                    if test_x_con < 0 || test_x_con >= MAP_WIDTH as i32 || test_y_con < 0 || test_y_con >= MAP_HEIGHT as i32 {
                         hit_wall = true; //Just set distance to maximum depth
                         distance_to_wall = depth;
                     } else {
                         // Ray is inbounds so test to see if the ray cell is a wall block
-                        let test_convert = test_y_con * MAP_WIDTH + test_x_con;
+                        let test_convert = test_y_con * MAP_WIDTH as i32 + test_x_con;
                         let test_con = test_convert as usize; //temporary cast for arithmetic sake
                         if map_slice[test_con] as char == '#' {
                             hit_wall = true;
@@ -143,11 +153,11 @@ fn main() {
                 }
 
                 //Calculate distance to ceiling and floor
-                let ceiling = (buff_height as f64 / 2.0) - (buff_height as f64 / distance_to_wall);
+                let ceiling = (buff_height as f64 / 2.0) - buff_height as f64 / distance_to_wall;
                 let ceiling_con = ceiling as usize; //temporary cast for arithmetic sake
-                let floor = buff_height - ceiling_con;
+                let floor: isize = buff_height as isize - ceiling_con as isize;
 
-                let mut shade = ' ';
+                let mut shade;
 
                 if distance_to_wall <= depth / 4.0 {
                     shade = '█';
@@ -163,14 +173,29 @@ fn main() {
 
                 for z in 0..buff_height {
                     if z <= ceiling_con {
-                        window_buffer[z * buff_width + i] = ' ' as u16;
-                    } else if z > ceiling_con && z <= floor {
+                        window_buffer[z * buff_width + i] = '=' as u16;
+                    } else if z > ceiling_con && z <= floor as usize {
                         window_buffer[z * buff_width + i] = shade as u16;
                     } else {
-                        window_buffer[z * buff_width + i] = ' ' as u16;
+                        // Shade floor based on distance
+                        let floor_distance = 1.0 - ((z as f64 - buff_height as f64 / 2.0) / (buff_height as f64 / 2.0));
+                        if floor_distance < 0.25 {
+                            shade = '#';
+                        } else if floor_distance < 0.5 {
+                            shade = 'x';
+                        } else if floor_distance < 0.75 {
+                            shade = '.';
+                        } else if floor_distance < 0.9 {
+                            shade = '-';
+                        } else {
+                            shade = ' ';
+                        }
+                        window_buffer[z * buff_width + i] = shade as u16;
                     }
                 }
             }
+
+            winuser::wsprintfW(buff_sec_ptr, s_ptr);
 
             wincon::WriteConsoleOutputCharacterW(hconsole, buffer_ptr, 120 * 40, buff_coord, &mut dw_bytes_written);
         }
